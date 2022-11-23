d70d3d3e238e087489aea24cf09c8eea3b0623df
Jude <jude.cowden@protonmail.com>
2022-11-23 10:08:29 +0000

    Setting up the main page

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
 # Imports the Flask object within the flask module
 from flask import Flask
+from flask import render_template
 
 # Initiates the Flask object and sets the 'static' folder as the template folder
 app = Flask(__name__, template_folder='static')
+
+
+# The route for the main page
+@app.route('/', methods=['GET'])
+def main():
+    return render_template(
+        'template.html'
+    )
 
 
 # Checks to see if the current file is the one being run (ie if another file
